61cfd6e9e20b6959bf0431e8ea6d99b10e3982a8
GoBluechipRebo <alex.revilla@gobluechip.ai>
2025-07-31 09:34:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const puppeteer = require('puppeteer');
-const PuppeteerHar = require('puppeteer-har');
-const fs = require('fs');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -17,24 +15,27 @@ app.post('/extract-security-id', async (req, res) => {
   }
 
   const url = `https://global.morningstar.com/es/inversiones/fondos/${isin}/cartera`;
-  const harPath = `har-${isin}.har`;
 
   try {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+
     const page = await browser.newPage();
-    const har = new PuppeteerHar(page);
+    let securityId = null;
 
-    await har.start({ path: harPath });
+    // Intercept network responses
+    page.on('response', async (response) => {
+      const reqUrl = response.url();
+      const match = reqUrl.match(/\/fund\/(?:portfolio|process)\/[^\/]+\/([^\/]+)\/data/);
+      if (match && match[1]) {
+        securityId = match[1];
+      }
+    });
+
     await page.goto(url, { waitUntil: 'networkidle0', timeout: 60000 });
-    await har.stop();
     await browser.close();
-
-    const harContent = fs.readFileSync(harPath, 'utf8');
-    fs.unlinkSync(harPath); // eliminar el archivo .har
-
-    const regex = /\/fund\/(?:portfolio|process)\/[^\/]+\/([^\/]+)\/data/;
-    const match = harContent.match(regex);
-    const securityId = match ? match[1] : null;
 
     res.json({
       isin,
